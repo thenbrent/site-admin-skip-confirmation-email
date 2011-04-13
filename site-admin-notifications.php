127ae44b006bb4da7da4e755b29f54d346d0a50d
Brent Shepherd <thenbrent@gmail.com>
2011-04-14 01:30:04 +0700

    Initial commit with working display of email notification.

---
+++ site-admin-notifications.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Site Admin Notifications
+Description: Allow vanilla site admins to add news users and not send a notification email. 
+Author: _FindingSimple
+Author URI: http://findingsimple.com/
+Version: 0.1
+*/
+
+/**
+ * When the calling function is the super admin check for email notifications on the 
+ * user-new.php page, add all admin users to the returns array so it will return true 
+ * for any admin user and display the email notification option. 
+ *
+ * A horrible hack that will break at the drop of a hat.
+ **/
+function sac_spoof_super_admin( $admins ){
+	global $pagenow;
+
+	if( $pagenow == 'user-new.php' ){
+		$stacktrace = array_pop( debug_backtrace() );
+		if( strpos( $stacktrace[ 'file' ], 'user-new.php' ) && in_array( $stacktrace[ 'line' ], array( 335, 251 ) ) && $stacktrace[ 'function' ] == 'is_super_admin' ){
+			$all_admins = array();
+			foreach( get_users( array( 'role' => 'administrator' ) ) as $user )
+				array_push( $all_admins, $user->user_login );
+			$admins = array_merge( $admins, $all_admins );
+			$admins = array_unique( $admins );
+			error_log( 'admins = ' . print_r( $admins, true ) );
+		}
+	}
+	return $admins;
+}
+add_filter( 'site_option_site_admins', 'sac_spoof_super_admin' );
